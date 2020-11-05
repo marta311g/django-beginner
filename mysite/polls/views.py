e34bb0bb1be92dea3c0c088681d56b44b7ee7d56
Marta Gračner <marta.gracner@fer.hr>
2020-11-05 20:12:58 +0100

    adding views that return real data from database

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is an index page')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = ", ".join(q.question_text for q in latest_questions)
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse('This is the detail view of question: %s' % question_id)
+
+def results(request, question_id):
+    return HttpResponse('Thiese are the results of the question: % s' % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Vote on question: % s' % question_id)
