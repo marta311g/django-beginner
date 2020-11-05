4f0a7037a9dc67e6d1129d0a3ef8faf79b92a6a9
Marta Gračner <marta.gracner@fer.hr>
2020-11-05 20:59:11 +0100

    adding more html templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 # Create your views here.
@@ -10,7 +10,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse('This is the detail view of question: %s' % question_id)
+    question = get_object_or_404(Question.objects.get(pk = question_id))
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse('Thiese are the results of the question: % s' % question_id)
