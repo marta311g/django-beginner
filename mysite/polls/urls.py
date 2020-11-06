b5e9ce15efc12712c34fe16a04c01506e931a2ec
Marta Gračner <marta.gracner@fer.hr>
2020-11-06 12:35:38 +0100

    fixing bugs in urls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
-    url(r'^(?P<question_id>[0-9]+)/results$', views.results, name="results"),
-    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name="vote"),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<question_id>[0-9]+)/results$', views.results, name='results'),
+    re_path(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name='vote'),
 ]
