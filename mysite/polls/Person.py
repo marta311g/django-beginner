605490aa4643f18d63fd9bd976230977fcc198fe
Marta Gračner <marta.gracner@fer.hr>
2020-12-06 15:09:14 +0100

    refactoring

--- mysite/polls/Person.py
+++ mysite/polls/Person.py
@@ -1,6 +1,6 @@
 from .db import Database
 
- 
+
 class Request:
     def __init__(self, label, datetime, timezone, method, url, status_code):
         self.label = label
@@ -20,76 +20,16 @@ def create_request(request):
     return db.query(query_string)
 
 
-# driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", ""))
-# session = driver.session()
-#
-# result = session.run("MATCH(a:Person) WHERE a.name = $name")
-# print(result.data())
-# result = session.run()
-#
-# def count_users(tx):
-#     result = tx.run("MATCH (a:User) RETURN count(a)")
-#     record = result.single()
-#     return record[0]
-#
-# num_users = session.read_transaction(count_users)
-
-# class Person:
-#     def __init__(self, uri, name, born):
-#         self.driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
-
-#     def close(self):
-#         self.driver.close()
-
-#     def num_users(self):
-#         with self.driver.session() as session:
-#             result = session.read_transaction(self.count_users)
-#             return result
-
-#     @staticmethod
-#     def count_users(tx):
-#         result = tx.run("MATCH (a:Person) RETURN count(a)")
-#         record = result.single()
-#         return record[0]
+class Parameter:
+    def __init__(self, label, left, right):
+        self.label = label
+        self.left = left
+        self.right = right
 
 
-# def test_neo():
-#     temp = Person("bolt://localhost:7687", "neo4j", "password")
-#     temp.num_users()
-#     temp.close()
+def create_parameter(parameter):
+    query_string = 'CREATE(' + parameter.label + ':Parameter {left: "' + parameter.left + ', right: "'
+    query_string += parameter.right + '"}) RETURN ' + parameter.label
 
-#uri = "bolt://localhost:7687"
-#driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
-
-
-
-#     def close(self):
-#         self.driver.close()
-
-#     def create_in_db(self):
-#         with self.driver.session() as session:
-#             return session.write_transaction(create_request, self)
-
-#     @staticmethod
-#     def create_request(tx, self):
-#         request = tx.run("CREATE($request_name:Request "
-#             "{datetime: $datetime, timezone: $timezone, method: $method, url: $url, status_code: $status_code}) "
-#             "RETURN $request_name", request_name=self.name, datetime=self.datetime, timezone=self.timezone,
-#             method=self.method, url=self.url, status_code=self.status_code)
-#         return request.single()[0]
-
-# class Parameter:
-#     def __init__(self, left, right):
-#         self.driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
-    
-#     def close(self):
-#         self.driver.close()
-    
-#     def create_in_db(self):
-#         with self.driver.session() as session:
-#             return session.write_transaction(create_parameter, self)
-    
-#     @staticmethod
-#     def create_parameters(tx, self):
-#         param = tx.run("CREATE($param_name:Parameter {left: $left, right: $right}) RETURN $param_name",
-#             param_name=self.name, left=self)
+    db = Database("bolt://localhost:7687", "neo4j", "password")
+    return db.query(query_string)
